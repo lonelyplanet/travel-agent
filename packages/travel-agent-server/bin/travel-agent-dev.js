fd30a465a3a1a49758807bf0be11363ce98f6b1e
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2017-10-25 16:01:13 -0500

    fix: Support debug mode in dev

--- packages/travel-agent-server/bin/travel-agent-dev.js
+++ packages/travel-agent-server/bin/travel-agent-dev.js
@@ -1,11 +1,19 @@
 const nodemon = require("nodemon");
 const path = require("path");
 const fs = require("fs");
+const program = require("commander");
+
+program
+.version("0.1.0")
+
+program
+  .option("-d, --debug", "run the nodejs inspector")
+  .description("run a dev server")
+  .parse(process.argv);
 
 const tsNodePath = fs.existsSync(path.join(__dirname, "../node_modules/.bin/ts-node")) ?
   path.join(__dirname, "../node_modules/.bin/ts-node") :
   path.join(process.cwd(), "./node_modules/.bin/ts-node");
-
 
 nodemon({
   ext: "js json ts tsx",
@@ -18,7 +26,7 @@ nodemon({
     ].map(p => path.join(process.cwd(), p)),
     path.join(__dirname, "../dist/**/*"),
   ],
-  exec: `${tsNodePath} ${path.join(process.cwd(), "app/index.ts")} `,
+  exec: `${tsNodePath} ${program.debug ? "--inspect" : ""} ${path.join(process.cwd(), "app/index.ts")} `,
 });
 
 nodemon.on("start", function () {
