10dbb13c1bf256c6550b86336591da8a48c9aa13
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2017-12-01 16:44:41 -0600

    Update index.js

--- packages/generator-travel-agent/generators/app/templates/config/index.js
+++ packages/generator-travel-agent/generators/app/templates/config/index.js
@@ -1,13 +1,5 @@
 module.exports = {
-  middleware: {
-    forceJsonContentType: (req, res, next) => {
-      if (req.originalUrl.indexOf(".json") > -1) {
-        req.headers["content-type"] = "application/json";
-      }
-
-      next();
-    },
-  },
+  middleware: [],
   webpack: {
     entry: {
       app: "./app/shared/client"
